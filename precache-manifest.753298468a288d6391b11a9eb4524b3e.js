a634350d5bb65ca660677b7d1cec3921d007138a
malay10 <malaybhakta10@gmail.com>
2020-07-04 13:11:58 -0500

    Updates

---
+++ precache-manifest.753298468a288d6391b11a9eb4524b3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227283b45a68621be5f08ed501a3ef1b",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "86ad64eba3a05608e1a6",
+    "url": "/smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f01b28916e5df4641413",
+    "url": "/smart-brain/static/css/main.33c75f91.chunk.css"
+  },
+  {
+    "revision": "86ad64eba3a05608e1a6",
+    "url": "/smart-brain/static/js/2.4a94e5a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brain/static/js/2.4a94e5a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f01b28916e5df4641413",
+    "url": "/smart-brain/static/js/main.b3de9e5b.chunk.js"
+  },
+  {
+    "revision": "6be7caf3921d67934d81",
+    "url": "/smart-brain/static/js/runtime-main.5a27d6c2.js"
+  }
+]);
